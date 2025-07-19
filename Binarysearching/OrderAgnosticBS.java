443aea00f8b8851f4ffa01a561fa6da5dd4af957
Anant Butola <anantbutola8@gmail.com>
2025-07-20 00:59:03 +0530

    Upload onl JAVA fo;de to GitHub

---
+++ Binarysearching/OrderAgnosticBS.java
@@ -0,0 +1,48 @@
+package Binarysearching;
+
+public class OrderAgnosticBS {
+    public static void main(String[] args) {
+         //int[] arr={2,4,5,7,9,11,15,19,22};
+         int[] arr={22,19,15,11,9,7,5,4,2};
+        int target=19;
+        int ans=BS(arr,target);
+        System.out.println(ans);
+    }
+
+        static int BS(int[] arr, int target) {
+        int str=0;
+        int end=arr.length -1;
+        boolean asc;
+        if (arr[str]<arr[end]) {
+            asc=true;
+        }else{
+            asc=false;
+        }
+        if (asc) {
+            while (str<=end) {
+            int mid=str+(end-str)/2;
+            if (arr[mid]==target) {
+                return mid;
+            }else if (arr[mid]>target) {
+                end=mid-1;
+            }else{
+                str=mid+1;
+            }
+        }
+        }else{
+            while (str<=end) {
+            int mid=str+(end-str)/2;
+            if (arr[mid]==target) {
+                return mid;
+            }else if (arr[mid]>target) {
+                str=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        }
+        return -1;
+    }
+
+}
+ 
