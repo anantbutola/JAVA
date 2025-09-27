c1ad1d3561dc2c6a7964f5f62ad28d435beef780
Anant Butola <anantbutola8@gmail.com>
2025-09-28 03:07:27 +0530

    exception handling

--- oops/lecture2/packages/pack2/greet.java
+++ oops/lecture2/packages/pack2/greet.java
@@ -4,7 +4,7 @@ public class greet {
     public static void main(String[] args) {
         
     }
-    public static void welcome(){
+    public  static void welcome(){
              System.out.println("welcome to java programming");
         }
 }
